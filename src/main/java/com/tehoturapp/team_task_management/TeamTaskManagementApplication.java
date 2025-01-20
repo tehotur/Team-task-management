d6d110cf399d1ebdfca62736d4bc14d545be2ab9
tehotur <erteplicek@gmail.com>
2025-01-20 17:59:51 +0100

    initial commit

---
+++ src/main/java/com/tehoturapp/team_task_management/TeamTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tehoturapp.team_task_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTaskManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tehoturapp/team_task_management/TeamTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tehoturapp.team_task_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
